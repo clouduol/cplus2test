420a4ab79fa44adbb97a376db61b537a1861b8b3
learnMachining <guoyunlong16@mails.ucas.ac.cn>
2018-03-14 15:42:15 +0800

    test base size

---
+++ size/base.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"char\t"<<sizeof(char)<<endl;
+	cout<<"short\t"<<sizeof(short)<<endl;
+	cout<<"unsigned short\t"<<sizeof(unsigned short)<<endl;
+	cout<<"int\t"<<sizeof(int)<<endl;
+	cout<<"unsigned int\t"<<sizeof(unsigned int)<<endl;
+	cout<<"long\t"<<sizeof(long)<<endl;
+	cout<<"unsigned long\t"<<sizeof(unsigned long)<<endl;
+	cout<<"long long\t"<<sizeof(long long)<<endl;
+	cout<<"unsigned long long\t"<<sizeof(unsigned long long)<<endl;
+	//cout<<"_int64\t"<<sizeof(_int64)<<endl;
+	cout<<"float\t"<<sizeof(float)<<endl;
+	cout<<"double\t"<<sizeof(double)<<endl;
+	cout<<"void*\t"<<sizeof(void*)<<endl;
+}
